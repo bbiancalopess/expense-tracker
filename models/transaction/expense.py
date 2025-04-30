015c54b6f283be6e91b17f54514abaa5b4ec91e3
lauar-m <lauar.m@proton.me>
2025-04-30 20:36:00 -0300

    add transaction classes

---
+++ models/transaction/expense.py
@@ -0,0 +1,9 @@
+from models.transaction.transaction import Transaction
+from datetime import date
+
+class Expense(Transaction):
+    def __init__(self, value: float, date: date, description: str, category: str, payment_method: str, id: int = None):
+        super().__init__(value, date, description, category, payment_method, id)
+
+    def tipo(self):
+        return "expense"
